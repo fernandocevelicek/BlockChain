f75ae7f138cba2b3a3112c5af66f086df0c36095
Fernando Cevelicek <cevelicekfernando@gmail.com>
2022-10-20 10:38:06 -0300

    First commit

---
+++ Laboratorio3/Block.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratorio3
+{
+    internal class Block
+    {
+
+        public int Index
+            { get; set; }
+        public DateTime TimeStamp { get; set; }
+        
+        public String PreviusHash { get; set; }
+
+        public String Hash { get; set; }
+
+        public String Data { get; set; }
+
+        public Block(DateTime timeStamp, String PreviusHash, String Data)
+        {
+            Index = 0;
+            TimeStamp = timeStamp;
+            PreviusHash = PreviusHash;
+            Data = Data;
+            Hash = CalculateHash();
+        }
+
+        public String CalculateHash()
+        {
+            SHA256 sHA256 = SHA256.Create();
+            byte[] inputBytes = Encoding.ASCII.GetBytes($"{ TimeStamp}-{ PreviusHash ?? ""}-{ Data}");
+            byte[] outputBytes = sHA256.ComputeHash(inputBytes);
+            return Convert.ToBase64String(outputBytes);
+        }
+    }
+}
---
+++ Laboratorio3/Program.cs
@@ -0,0 +1,29 @@
+﻿using Laboratorio3;
+using System;
+using System.Diagnostics;
+using System.Net.Http.Json;
+using System.Security.Claims;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Xml;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+        BlockChain diario = new BlockChain();
+
+        diario.AddBlock(new Block(DateTime.Now, null, "{sender: Banco Nacion, receiver: Fernando BNA, Monto: $520}"));
+        diario.AddBlock(new Block(DateTime.Now, null, "{sender: Minati.Mp, receiver: Fernando BNA, Monto: $1.400}"));
+        diario.AddBlock(new Block(DateTime.Now, null, "{sender: Ofarina.Mp, receiver: Fernando BNA, Monto: $680}"));
+        diario.AddBlock(new Block(DateTime.Now, null, "{sender: Torres.Mp, receiver: Fernando BNA, Monto: $3.400}"));
+        string strserialize = JsonSerializer.Serialize<BlockChain>(diario);
+        Console.WriteLine(strserialize);
+
+    }
+}
+
+
+
+
